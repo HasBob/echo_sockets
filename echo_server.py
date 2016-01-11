caf28f2bbc65ec965f8ecbc34086ceab06b47f50
Bob Jenkins <bob@hasoffers.com>
2016-01-10 22:43:46 -0800

    echo client and server working

--- echo_server.py
+++ echo_server.py
@@ -7,35 +7,38 @@ def server(log_buffer=sys.stderr):
     address = ('127.0.0.1', 10000)
     # TODO: Replace the following line with your code which will instantiate
     #       a TCP socket with IPv4 Addressing, call the socket you make 'sock'
-    sock = None
+    sock = socket.socket(
+        socket.AF_INET,
+        socket.SOCK_STREAM,
+        socket.IPPROTO_IP)
     # TODO: You may find that if you repeatedly run the server script it fails,
     #       claiming that the port is already used.  You can set an option on
     #       your socket that will fix this problem. We DID NOT talk about this
     #       in class. Find the correct option by reading the very end of the
     #       socket library documentation:
     #       http://docs.python.org/3/library/socket.html#example
-
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     # log that we are building a server
     print("making a server on {0}:{1}".format(*address), file=log_buffer)
-
     # TODO: bind your new sock 'sock' to the address above and begin to listen
     #       for incoming connections
+    sock.bind(address)  
 
     try:
         # the outer loop controls the creation of new connection sockets. The
         # server will handle each incoming connection one at a time.
         while True:
             print('waiting for a connection', file=log_buffer)
-
             # TODO: make a new socket when a client connects, call it 'conn',
             #       at the same time you should be able to get the address of
             #       the client so we can report it below.  Replace the
             #       following line with your code. It is only here to prevent
             #       syntax errors
-            addr = ('bar', 'baz')
+            sock.listen(1)
+            conn, addr = sock.accept()    #addr = ('bar', 'baz')
+            
             try:
                 print('connection - {0}:{1}'.format(*addr), file=log_buffer)
-
                 # the inner loop will receive messages sent by the client in
                 # buffers.  When a complete message has been received, the
                 # loop will exit
@@ -45,19 +48,23 @@ def server(log_buffer=sys.stderr):
                     #       following line with your code.  It's only here as
                     #       a placeholder to prevent an error in string
                     #       formatting
-                    data = b''
+                    data = conn.recv(16)    #data = b''
                     print('received "{0}"'.format(data.decode('utf8')))
                     # TODO: Send the data you received back to the client, log
                     # the fact using the print statement here.  It will help in
                     # debugging problems.
-                    print('sent "{0}"'.format(data.decode('utf8')))
+                    conn.sendall(data)
+                    print('sent "{0}"'.format(data.decode('utf8')), file=log_buffer)
                     # TODO: Check here to see if the message you've received is
                     # complete.  If it is, break out of this inner loop.
+                    if len(data) < 16:
+                        break
 
             finally:
                 # TODO: When the inner loop exits, this 'finally' clause will
                 #       be hit. Use that opportunity to close the socket you
                 #       created above when a client connected.
+                conn.close()
                 print(
                     'echo complete, client connection closed', file=log_buffer
                 )
@@ -67,7 +74,7 @@ def server(log_buffer=sys.stderr):
         #       close the server socket and exit from the server function.
         #       Replace the call to `pass` below, which is only there to
         #       prevent syntax problems
-        pass
+        sock.close()    #pass
         print('quitting echo server', file=log_buffer)
 
 
